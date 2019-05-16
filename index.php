4c0410f14ea8e89c6d9d21637ffaff9d5ebbf00a
ameernasser88 <34694069+ameernasser88@users.noreply.github.com>
2019-05-17 01:18:01 +0200

    styling

--- index.php
+++ index.php
@@ -45,8 +45,31 @@ if (isset($_SESSION['loggedin'])) {
       <!--stylesheets-->
       <link href="css/style.css" rel='stylesheet' type='text/css' media="all">
       <!--//style sheet end here-->
-      <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet">
-      <link rel="stylesheet" href="css/bootstrap.css">
+      <link href="css/stylesheet.css" rel="stylesheet" type="text/css">
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+
+<!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+
+<script type="text/javascript">
+  $(function() {
+    $('#datetimepicker1').datetimepicker({
+      language: 'pt-BR'
+    });
+  });
+</script>
+  
    </head>
    <body>
       <div class="mid-class">
@@ -98,7 +121,7 @@ if (isset($_SESSION['loggedin'])) {
 
                          <div class="form-left-to-w3l" style="margin-top: 25px; margin-bottom: 25px; ">
                            <div style="float: left; margin-bottom: 15px;">Birth Date :</div> 
-                           <input type="date" name="birthdate" required="required">
+                           <input  type="date"  name="birthdate" required="required">
                         </div>
 
 <div   style="margin-top: 30px; height: 20px; float: inherit;">

53c50737d7d680f731aa89ab7936b3ee45198878
ameernasser88 <34694069+ameernasser88@users.noreply.github.com>
2019-05-17 04:10:54 +0200

    search bar

--- editprofile.php
+++ editprofile.php
@@ -177,7 +177,7 @@ width: 125px;
 
  <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn  my-2 my-sm-0" style="background-color: #00a1ff; border-color: white; color: white;" type="submit">Search</button>
+      <button class="btn  my-2 my-sm-0" style="background-color: #00a1ff; border-color: white; color: white;" type="submit"><i class="fa fa-search"></i></button>
     </form>
     </ul>
    
--- home.php
+++ home.php
@@ -184,7 +184,7 @@ height: 150px;
 
  <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn  my-2 my-sm-0" style="background-color: #00a1ff; border-color: white; color: white;" type="submit">Search</button>
+      <button class="btn  my-2 my-sm-0" style="background-color: #00a1ff; border-color: white; color: white;" type="submit"><i class="fa fa-search"></i></button>
     </form>
     </ul>
    

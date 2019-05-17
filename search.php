16615a2c1c41143ee0655b48847f626053c70b77
ameernasser88 <34694069+ameernasser88@users.noreply.github.com>
2019-05-17 23:45:21 +0200

    header clear cache in search page

--- search.php
+++ search.php
@@ -1,6 +1,7 @@
 <?php
 
 session_start();
+header('Cache-Control: no cache');
 // If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.php');

8d10332690046996cf48073e1956186ebc13dd6e
sefdf01 <34137064+sefdf01@users.noreply.github.com>
2019-05-19 14:18:28 +0200

    profile page

--- home.php
+++ home.php
@@ -54,18 +54,23 @@ $homeTown=NULL;
 
 $maritalStatus = NULL;
 
-$result2 = $con->query("SELECT nickName , phone, profilePicture , homeTown , maritalStatus FROM users  where id = $id ");
+$birthdate = NULL;
+
+$result2 = $con->query("SELECT nickName , phone, profilePicture , homeTown , maritalStatus , birthdate FROM users  where id = $id ");
 
  while ($row = $result2->fetch_assoc())
  {
  $nickname = $row['nickName'];
 $profilepicture = $row['profilePicture'];
 
-$phone =  $row['phone'];;
+$phone =  $row['phone'];
 
-$homeTown= $row['homeTown'];;
+$homeTown= $row['homeTown'];
 
-$maritalStatus =  $row['maritalStatus'];;
+$maritalStatus =  $row['maritalStatus'];
+
+$birthdate = $row['birthdate'];
+
  }
 
 
@@ -121,7 +126,18 @@ if( $nickname != NULL )
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 
-
+<!-- Stylesheets
+    ================================================= -->
+		<link rel="stylesheet" href="style\bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="style\icon.css">
+		<link rel="stylesheet" href="style\loader.css">
+		<link rel="stylesheet" href="style\idangerous.swiper.css">
+		<link rel="stylesheet" href="style\stylesheet.css">
+    <!--Google Webfont-->
+		<link href='https://fonts.googleapis.com/css?family=Raleway:400,100,100italic,200,200italic,300,300italic,400italic,500,500italic,600,600italic,700' rel='stylesheet' type='text/css'>
+    <!--Favicon-->
+    <link rel="shortcut icon" type="image/png" href="images/fav.png"/>
 
 
 
@@ -130,8 +146,9 @@ if( $nickname != NULL )
 <style>
 	
 	.avatar-pic {
-width: 150px;
-height: 150px;
+width: 70%;
+height: 70%;
+display:inline;
 }
 
 
@@ -202,49 +219,87 @@ height: 150px;
 
 
 
-		<div class="content" style="margin-top:2%; margin-bottom: 0;" >
-			<!-- <h4>Welcome , <?=$name?> !</h4> -->
-
-
-
-
-
+<div class="content" style="margin-top:2%; margin-bottom: 0;" >
 
 <div class="container bootstrap snippet">
+  
     <div class="row">
         <div class="col-sm-10">
-            <h1><?=$name?></h1></div>
-        <div class="col-sm-2">
-            <a href="#" class="pull-right"><img title="profile image" class="img-circle img-responsive avatar-pic" src="profilepictures/<?=$profilepicture?>"></a>
-        </div>
+        
     </div>
     <div class="row">
-        <div class="col-sm-3">
-            <!--left col-->
-
-            <ul class="list-group">
-                <li class="list-group-item text-muted">Info</li>
-                <li class="list-group-item text-right"><span class="pull-left"><strong>Hometown   </strong></span><?=$homeTown?></li>
-                <li class="list-group-item text-right"><span class="pull-left"><strong>Phone   </strong></span> <?=$phone?></li>
-                <li class="list-group-item text-right"><span class="pull-left"><strong>Status   </strong></span> <?=$maritalStatus?></li>
-
-            </ul>
-
-          
-
-           
-
-            
-
-        </div>
-
-
-
-
+    <div class="col-xs-12 col-md-4 left-feild">
+					<div class="be-user-block style-3">
+          <h1 class="text-center"><?=$name?></h1></div>
+						<div class="be-user-detail">
+            <div class="profilePicture">
+               <a href="#" class="pull-left text-center"><img title="profile image" class="img-circle img-responsive avatar-pic" src="profilepictures/<?=$profilepicture?>" style="box-shadow:0 2px 4px rgba(0,0,0,0.5);"></a>
+            </div>
+							<a class="btn btn-primary buttons" id="followbtn" style="margin:7% 0px; color:white;"><i class="fa fa-plus light"></i>Follow</a>
+							<div class="btn btn-danger" id="messagebtn" style="margin:7% 0px;">
+								<i class="fa fa-envelope-o"></i>message
+								<div class="message-popup">
+									<div class="message-popup-inner container"> 
+										<div class="row">
+											<div class="col-xs-12 col-sm-8 col-sm-offset-2">
+												<i class="fa fa-times close-button"></i>
+												<h5 class="large-popup-title">Send Message to <?=$name?></h5>
+												<div class="form-group">
+													<textarea class="form-input" required="" placeholder="Your text"></textarea>
+												</div>
+												<a class="btn btn-right color-1 size-1 hover-1">send message</a>	
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							
+							<div class="be-text-tags style-2">
+								<a href="page1.html">UI/UX</a>,
+								<a href="page1.html">Web Design</a>,
+								<a href="page1.html">Art Direction</a>
+							</div>
+							<div class="be-user-social">							
+								<a class="social-btn color-1" href="page1.html"><i class="fa fa-facebook"></i></a>
+								<a class="social-btn color-2" href="page1.html"><i class="fa fa-twitter"></i></a>
+								<a class="social-btn color-3" href="page1.html"><i class="fa fa-google-plus"></i></a>
+								<a class="social-btn color-4" href="page1.html"><i class="fa fa-pinterest-p"></i></a>
+								<a class="social-btn color-5" href="page1.html"><i class="fa fa-instagram"></i></a>
+								<a class="social-btn color-6" href="page1.html"><i class="fa fa-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="be-user-statistic" style="margin-left:auto; margin-right:auto; box-shadow:0 2px 4px rgba(0,0,0,0.5); color:blue">
+							<div class="stat-row clearfix"><i class="stat-icon fas fa-home"></i><strong>Hometown</strong><span class="stat-counter"><?=$homeTown?></span></div>
+							<div class="stat-row clearfix"><i class="stat-icon fas fa-heart"></i><strong>Status</strong><span class="stat-counter"><?=$maritalStatus?></span></div>
+							<div class="stat-row clearfix"><i class="stat-icon fas fa-phone"></i><strong>Phone</strong><span class="stat-counter"><?=$phone?></span></div>
+							<div class="stat-row clearfix"><i class="stat-icon fas fa-birthday-cake"></i><strong>Birthdate</strong><span class="stat-counter"><?=$birthdate?></span></div>
+						</div>
+					
+					<div class="be-desc-block">
+						<div class="be-desc-author">
+							<div class="be-desc-label text-center">About Me</div>
+							<div class="be-desc-text">
+								Nam sit amet massa commodo, tristique metus at, consequat turpis. In vulputate justo at auctor mollis. Aliquam non sagittis tortor. Duis tristique suscipit risus, quis facilisis nisl congue vitae. Nunc varius pellentesque scelerisque. Etiam quis massa vitae lectus placerat ullamcorper pellentesque vel nisl.
+							</div>
+						</div>
+						<div class="be-desc-author">
+							<div class="be-desc-label text-center">My Values</div>
+							<div class="be-desc-text">
+								Sed dignissim scelerisque pretium. Vestibulum vel lacus laoreet nunc fermentum maximus. Proin id sodales sem, at consectetur urna. Proin vestibulum, erat a hendrerit sodales, nulla libero ornare dolor.
+							</div>
+						</div>
+						<div class="be-desc-author">
+							<div class="be-desc-label text-center">My Skills</div>
+							<div class="be-desc-text">
+								Praesent pharetra eget ante nec sodales. Sed et orci sit amet justo lobortis luctus. Curabitur sit amet congue purus. Sed arcu lectus, suscipit in finibus id, consequat sagittis arcu.
+							</div>
+						</div>
+					</div>										
+				</div>
 
 		</div>
 
-		
+
 
 	<div class="content" style="margin: 0 auto;">
 
@@ -257,7 +312,8 @@ height: 150px;
     text-align: center;
     margin-left: auto;
     margin-right: auto;
-    margin-top:20%;">
+    margin-top:20%;
+    background-color:#f3f4f7">
   
   <p>2019  ©</p>
   
@@ -268,7 +324,12 @@ height: 150px;
 
 </div>
 
-
+<script src="script\jquery-2.1.4.min.js"></script>
+	<script src="script\bootstrap.min.js"></script>		
+	<script src="script\idangerous.swiper.min.js"></script>
+	<script src="script\isotope.pkgd.min.js"></script>	
+	<script src="script\jquery.viewportchecker.min.js"></script>	
+	<script src="script\global.js"></script>
 
 </body>
 </html>
